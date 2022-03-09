882eff3beefa75506b9e8e46d9c315e91fe3797e
Mustafa <60783369+PiusFaucus-Projects@users.noreply.github.com>
2022-03-09 12:11:52 +0000

    Add files via upload

--- views.py
+++ views.py
@@ -49,6 +49,39 @@ def menu():
     return _redirect_welcome()
 
 
+# TypeError: organizing() missing 1 required positional argument: 'response'
+
+@app.route('/organizing', methods=['GET', 'POST'])
+def organizing():
+    response = VoiceResponse()
+    with response.gather(
+            num_Digits=1, action=url_for('organizing_menu'), method="POST"
+    ) as g:
+        g.say("To call the Western press 1. To call the " +
+              "Central press 2. To call the  South Eastern press 3 " +
+              "To call the Statewide General Campaign Questions press 4.",
+              voice="alice", language="en-GB", loop=3)
+
+    return response
+
+
+@app.route('/organizing_menu', methods=['GET', 'POST'])
+def organizing_menu():
+    selected_option = request.form['Digits']
+    option_actions = {'1': western_press,
+                      '2': central_press,
+                      '3': south_eastern_press,
+                      '4': statewide_general_campaign_questions}
+
+    if option_actions in selected_option:
+        response = VoiceResponse()
+        option_actions[selected_option](response)
+        return twiml(response)
+
+    return _redirect_welcome()
+
+
+'''''
 # This bit cant figure it yet it gives an error.
 # werkzeug.exceptions.BadRequestKeyError: 400 Bad Request: The browser (or proxy) sent a
 # request that this server could not understand.
@@ -57,10 +90,10 @@ def menu():
 @app.route('/organizing', methods=['GET', 'POST'])
 def organizing():
     selected_option = request.form['Digits']
-    option_actions = {'2': "+12024173312",
-                      '3': "+12024173378",
-                      '4': "+12027336386",
-                      "5": "+12027336637"}
+    option_actions = {'1': "+12024173312",
+                      '2': "+12024173378",
+                      '3': "+12027336386",
+                      "4": "+12027336637"}
 
     if selected_option in option_actions:
         response = VoiceResponse()
@@ -68,6 +101,7 @@ def organizing():
         return twiml(response)
 
     return _redirect_welcome()
+'''''
 
 
 # private methods
@@ -87,9 +121,35 @@ def spanish(response):
     return response
 
 
+def western_press(response):
+    response.dial("number goes here")
+    return response
+
+
+def central_press(response):
+    response.dial("number goes here")
+    return response
+
+
+def central_press(response):
+    response.dial("number goes here")
+    return response
+
+
+def south_eastern_press(response):
+    response.dial("number goes here")
+    return response
+
+
+def statewide_general_campaign_questions(response):
+    response.dial("number goes here")
+    return response
+
+
+'''''
 def organizing_menu(response):
     with response.gather(
-            numDigits=1, action=url_for('organizing'), method="POST"
+            num_Digits=1, action=url_for('organizing'), method="POST"
     ) as g:
         g.say("To call the Western press 1. To call the " +
               "Central press 2. To call the  South Eastern press 3 " +
@@ -97,9 +157,11 @@ def organizing_menu(response):
               voice="alice", language="en-GB", loop=3)
 
     return response
+'''''
 
 
 # This code returns all the way to welcome message
+
 def _redirect_welcome():
     response = VoiceResponse()
     response.say("Returning to the main menu", voice="alice", language="en-GB")
