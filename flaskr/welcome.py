397d1684baf15826289b39d8a01bbb55e3d6d7f3
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-12 17:10:48 +0100

    refactor as blueprint

---
+++ flaskr/welcome.py
@@ -0,0 +1,42 @@
+from flask import (
+         Blueprint, request,  url_for,
+        )
+from twilio.twiml.voice_response import VoiceResponse, Gather
+
+# all routes in this blueprint are pre-pended with '/welcome'
+bp = Blueprint('welcome', __name__, url_prefix='/welcome')
+
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    """Respond to incoming phone calls with a menu of options."""
+    # Start our TwinML response
+    resp = VoiceResponse()
+
+    # Start our <gather> verb
+    gather = Gather(num_digits=1, action='/gather')
+    gather.say('Welcome to Straight Ahead. To contact fundraising, press "1", to contact a regional organizer, press "2".')
+    resp.append(gather)
+
+    # If the user doesn't select an option, redirect
+    resp.redirect(url_for('welcome.index'))
+
+    return str(resp)
+
+@bp.route("/gather", methods=['GET', 'POST'])
+def gather():
+    # Start our TwinML response
+    resp = VoiceResponse()
+    selected_option = request.values['Digits']
+    option_actions = {'1': '/fundraising',
+                      '2': '/organizing_menu',
+                      }
+
+    if selected_option in option_actions:
+        response = VoiceResponse()
+        choice = option_actions[selected_option]
+        response.redirect(choice)
+        return str(response)
+
+    resp.redirect(url_for('welcome.index'))
+
+    return str(resp)
