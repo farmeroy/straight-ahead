d4394d4998ba2a4d5633353443e5f3f37de012e5
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-12 17:10:17 +0100

    remove endpoints, import blueprints

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -13,58 +13,8 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    @app.route("/voice", methods=['GET', 'POST'])
-    def voice():
-        """Respond to incoming phone calls with a menu of options."""
-        # Start our TwinML response
-        resp = VoiceResponse()
 
-        # Start our <gather> verb
-        gather = Gather(num_digits=1, action='/gather')
-        gather.say('Welcome to Straight Ahead. To contact fundraising, press "1", to contact a regional organizer, press "2".')
-        resp.append(gather)
-
-        # If the user doesn't select an option, redirect
-        resp.redirect('/voice')
-
-        return str(resp)
-
-    @app.route("/gather", methods=['GET', 'POST'])
-    def gather():
-        # Start our TwinML response
-        resp = VoiceResponse()
-        selected_option = request.values['Digits']
-        option_actions = {'1': '/fundraising',
-                          '2': '/organizing_menu',
-                          }
-
-        if selected_option in option_actions:
-            response = VoiceResponse()
-            choice = option_actions[selected_option]
-            response.redirect(choice)
-            return str(response)
-
-        resp.redirect('/voice')
-
-        return str(resp)
        
-    @app.route('/organizing_menu', methods=['GET', 'POST'])
-    def organizing_menu():
-        resp = VoiceResponse()
-        resp.say('You have reached regional organizers menu.')
-        # selected_option = request.form['Digits']
-        # option_actions = {'1': western_press,
-        #                   '2': central_press,
-        #                   '3': south_eastern_press,
-        #                   '4': statewide_general_campaign_questions}
-
-        # if option_actions in selected_option:
-        #     response = VoiceResponse()
-        #     option_actions[selected_option](response)
-        #     return twiml(response)
-
-        resp.redirect('/voice')
-        return str(resp)
 
     @app.route('/fundraising', methods=['GET', 'POST'])
     def fundraising():
@@ -73,7 +23,14 @@ def create_app(test_config=None):
         
         resp.redirect('/voice')
         return str(resp)
+    
+    from . import welcome
+    app.register_blueprint(welcome.bp)
 
+    from . import regional
+    app.register_blueprint(regional.bp)
+
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
