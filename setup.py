9b394a1f5a59196f74d983df4f87c97cc60690ac
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-04-04 17:45:12 +0200

    removed build, reconfigured straight-ahead-hotline to straight_ahead_hotline

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-        name='straight-ahead-hotline',
+        name='straight_ahead_hotline',
         version='1.0.0',
         packages=find_packages(),
         include_package_data=True,
         zip_safe=False,
         install_requires=[
-            'straight-ahead-hotline',
+            'straight_ahead_hotline',
             ],
         )
 
